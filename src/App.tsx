b88ce6e1c3bfa491fc53ef80108e1b290edd6edc
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-01-08 20:22:48 -0300

    Start development

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <main className="w-full  h-screen bg-black gap-0 flex items-center justify-center  ">
+      <h1 className="text-white ">hello world</h1>
+    </main>
+  )
+}
