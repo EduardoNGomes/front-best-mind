56201fc0314427b4dcf18ac9a3d45d3a2e2824e5
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-01-09 19:42:32 -0300

    feat: add textarea-with-label

---
+++ src/components/ui/textarea-with-label .tsx
@@ -0,0 +1,31 @@
+import { ComponentPropsWithoutRef } from 'react'
+import { Label } from './label'
+import { Textarea } from './textarea'
+
+type TextareaWithLabelProps = ComponentPropsWithoutRef<'textarea'> & {
+  label: string
+  subtitle?: string
+}
+
+export function TextareaWithLabel({
+  label,
+  id,
+  placeholder,
+  subtitle,
+  ...props
+}: TextareaWithLabelProps) {
+  return (
+    <div className="w-full items-center gap-1.5">
+      <Label htmlFor={id}>{label}</Label>
+      {subtitle && (
+        <p className="text-zinc-600 text-sm font-normal">{subtitle}</p>
+      )}
+      <Textarea
+        id={id}
+        placeholder={placeholder}
+        className="resize-none h-40"
+        {...props}
+      />
+    </div>
+  )
+}
