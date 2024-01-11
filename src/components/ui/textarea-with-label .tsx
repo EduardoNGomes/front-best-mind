77785289c7dd4353c096a6687be920071c7a7190
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-01-11 20:40:13 -0300

    chore:refact component textarea-with-label

--- src/components/ui/textarea-with-label .tsx
+++ src/components/ui/textarea-with-label .tsx
@@ -1,19 +1,39 @@
-import { ComponentPropsWithoutRef } from 'react'
+import { ComponentProps, forwardRef } from 'react'
 import { Label } from './label'
 import { Textarea } from './textarea'
 
-type TextareaWithLabelProps = ComponentPropsWithoutRef<'textarea'> & {
+type TextareaWithLabelProps = ComponentProps<'textarea'> & {
   label: string
   subtitle?: string
 }
 
-export function TextareaWithLabel({
-  label,
-  id,
-  placeholder,
-  subtitle,
-  ...props
-}: TextareaWithLabelProps) {
+// export function TextareaWithLabel({
+//   label,
+//   id,
+//   placeholder,
+//   subtitle,
+//   ...props
+// }: TextareaWithLabelProps) {
+//   return (
+//     <div className="w-full items-center gap-1.5">
+//       <Label htmlFor={id}>{label}</Label>
+//       {subtitle && (
+//         <p className="text-zinc-600 text-sm font-normal">{subtitle}</p>
+//       )}
+//       <Textarea
+//         id={id}
+//         placeholder={placeholder}
+//         className="resize-none h-40 bg-neutral-100 rounded-lg border border-slate-200"
+//         {...props}
+//       />
+//     </div>
+//   )
+// }
+
+export const TextareaWithLabel = forwardRef<
+  HTMLInputElement,
+  TextareaWithLabelProps
+>(({ label, id, placeholder, subtitle, ...props }, ref) => {
   return (
     <div className="w-full items-center gap-1.5">
       <Label htmlFor={id}>{label}</Label>
@@ -24,8 +44,12 @@ export function TextareaWithLabel({
         id={id}
         placeholder={placeholder}
         className="resize-none h-40 bg-neutral-100 rounded-lg border border-slate-200"
+        // @ts-expect-error 'wrong ref type'
+        ref={ref}
         {...props}
       />
     </div>
   )
-}
+})
+
+TextareaWithLabel.displayName = 'TextareaWithLabel'
